742c43d537b3168aabf938f530ef8b74154f02c5
Ilya <inakinagiw733@mail.ru>
2026-08-19 02:03:39 +0400

    test

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-	build: {
-		cssTarget: 'chrome100', // Запрещает esbuild превращать стандартный CSS в устаревшие префиксы
-	},
-})
