5cd04138de8ea047e1920f25bc82412ce201d2ac
Ilya <inakinagiw733@mail.ru>
2026-08-19 02:16:25 +0400

    adaptive vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,34 @@
+import fs from 'fs'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+
+function getHtmlInputs(dir = __dirname, entries = {}) {
+	const files = fs.readdirSync(dir, { withFileTypes: true })
+
+	for (const file of files) {
+		if (file.isDirectory()) {
+			if (!['node_modules', 'dist', '.git', '.github'].includes(file.name)) {
+				getHtmlInputs(resolve(dir, file.name), entries)
+			}
+		} else if (file.isFile() && file.name.endsWith('.html')) {
+			const fullPath = resolve(dir, file.name)
+			const entryName = fullPath
+				.replace(__dirname, '')
+				.replace(/^[/\\]/, '')
+				.replace(/[/\\]/g, '_')
+				.replace('.html', '')
+
+			entries[entryName] = fullPath
+		}
+	}
+
+	return entries
+}
 
 export default defineConfig({
 	build: {
 		rollupOptions: {
-			input: {
-				main: resolve(__dirname, 'index.html'),
-				privacy: resolve(__dirname, 'privacy.html'),
-				notfound: resolve(__dirname, '404.html'),
-				projects: resolve(__dirname, 'projects/index.html'),
-			},
+			input: getHtmlInputs(),
 		},
 	},
 })
